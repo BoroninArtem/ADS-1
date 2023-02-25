14a5e4105ec334729eeeefec6cce5ee4caa9f717
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-02-25 21:03:27 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,8 @@
 #include <cstdint>
 #include "alg.h"
 #include <vector>
-
-
-bool checkPrime(uint64_t value) {
+bool checkPrime(uint64_t value)
+{
 	bool chek = true;
 	for (int i = 2; i < value; ++i) {
 		if (value % i == 0) {
@@ -18,7 +17,8 @@ bool checkPrime(uint64_t value) {
 	return chek;
 }
 
-uint64_t nPrime(uint64_t n) {
+uint64_t nPrime(uint64_t n)
+{
 	std::vector <int> arr;
 	for (int i = 2; i < 1000; ++i) {
 		if (checkPrime(i))
@@ -27,19 +27,22 @@ uint64_t nPrime(uint64_t n) {
 	return arr[n - 1];
 }
 
-uint64_t nextPrime(uint64_t value) {
-	for (int i = 1; i < 1000; ++i) {
+uint64_t nextPrime(uint64_t value)
+{
+	for (int i = 1; i < 1000; ++i)
+	{
 		if (value == nPrime(i))
 			return(nPrime(i + 1));
 	}
 }
 
-uint64_t sumPrime(uint64_t hbound) {
+uint64_t sumPrime(uint64_t hbound)
+{
 	uint64_t sum = 0;
-	for (int i = 1; i < hbound; ++i) {
-		if (hbound > nPrime(i)) {
+	for (int i = 1; i < hbound; ++i)
+	{
+		if (hbound > nPrime(i))
 			sum += nPrime(i);
-		}
 	}
 	return sum;
 }
