63cc021e6b346054f57b74f1844fa74c1c3e3caf
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-02-25 20:28:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,42 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	bool chek = true;
+	for (int i = 2; i < value; ++i) {
+		if (value % i == 0) {
+			chek = false;
+			break;
+		}
+		else {
+			chek = true;
+		}
+	}
+	return chek;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	vector <int> arr;
+	for (int i = 2; i < 1000; ++i) {
+		if (checkPrime(i))
+			arr.push_back(i);
+	}
+	return arr[n-1];
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	for (int i =1; i < 1000; ++i) {
+		if (value == nPrime(i))
+			return(nPrime(i + 1));
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for (int i = 1; i < hbound;++i) {
+		if (hbound > nPrime(i)) {
+			cout << nPrime(i) << endl;
+			sum += nPrime(i);
+		}
+	}
+	return sum;
 }
