a931cddf0d92603b1a0f4480ca11478077cc98db
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-02-25 21:41:52 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,45 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#include <vector>
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	bool chek = true;
+	for (int i = 2; i < value; ++i) {
+		if (value % i == 0) {
+			chek = false;
+			break;
+		}
+		else {
+			chek = true;
+		}
+	}
+	return chek;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	std::vector <int> arr;
+	for (int i = 2; i < 1000; ++i) {
+		if (checkPrime(i))
+			arr.push_back(i);
+	}
+	return arr[n - 1];
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	for (int i = 1; i < 1000; ++i) {
+		if (value == nPrime(i))
+			return nPrime(i + 1);
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for (int i = 1; i < hbound; ++i) {
+		if (hbound > nPrime(i)) {
+			sum += nPrime(i);
+		}
+	}
+	return sum;
 }
