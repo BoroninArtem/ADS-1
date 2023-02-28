24f8d04ae9f6e121ed40c8b6027d42a479c26ac1
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-02-28 15:55:42 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,64 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    bool chek = true;
+    if (value == 2)
+        return chek;
+    for (int i = 2; i < value; ++i) {
+        if (value % i == 0)
+            chek = false;
+    }
+    return chek;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t  chislo = 1;
+    int num = 0;
+    while (true) {
+        bool chek = true;
+        ++chislo;
+        if (chislo != 2) {
+            for (int i = 2; i < chislo; ++i) {
+                if (chislo % i == 0)
+                    chek = false;
+            }
+        }
+        if (chek == true)
+            num++;
+        if (num == n)
+            return chislo;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    uint64_t chislo = value;
+    if (value < 2)
+        return 2;
+    while (true) {
+        bool chek = true;
+        ++chislo;
+        for (int i = 2; i < chislo; ++i) {
+            if (chislo % i == 0)
+                chek = false;
+        }
+        if (chek == true)
+            return chislo;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    if (hbound < 3)
+        return 0;
+    for (int i = 2; i < hbound; ++i) {
+        bool chek = true;
+        for (int j = 2; j < i; ++j) {
+            if (i % j == 0)
+                chek = false;
+        }
+        if (chek == true) {
+            sum += i;
+        }
+    }
+    return sum;
 }
