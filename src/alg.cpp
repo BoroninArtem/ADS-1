05ed753c42ba9b9d32fbf8550e6bf5ec1963da48
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-03-25 23:08:20 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,48 +1,66 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <vector>
-bool checkPrime(uint64_t value)
-{
-	bool chek = true;
-	for (int i = 2; i < value; ++i) {
-		if (value % i == 0) {
-			chek = false;
-			break;
-		}
-		else {
-			chek = true;
-		}
-	}
-	return chek;
+
+bool checkPrime(uint64_t value) {
+    bool chek = true;
+    if (value == 2)
+        return chek;
+    for (int i = 2; i < value; ++i) {
+        if (value % i == 0)
+            chek = false;
+    }
+    return chek;
 }
 
-uint64_t nPrime(uint64_t n)
-{
-	std::vector <int> arr;
-	for (int i = 2; i < 1000; ++i) {
-		if (checkPrime(i))
-			arr.push_back(i);
-	}
-	return arr[n - 1];
+uint64_t nPrime(uint64_t n) {
+    uint64_t  chislo = 1;
+    int num = 0;
+    while (true) {
+        bool chek = true;
+        ++chislo;
+        if (chislo != 2) {
+            for (int i = 2; i < chislo; ++i) {
+                if (chislo % i == 0)
+                    chek = false;
+            }
+        }
+        if (chek == true)
+            num++;
+        if (num == n)
+            return chislo;
+    }
 }
 
-uint64_t nextPrime(uint64_t value)
-{
-	for (int i = 1; i < 1000; ++i)
-	{
-		if (value == nPrime(i))
-			return(nPrime(i + 1));
-	}
+uint64_t nextPrime(uint64_t value) {
+    uint64_t chislo = value;
+    if (value < 2)
+        return 2;
+    while (true) {
+        bool chek = true;
+        ++chislo;
+        for (int i = 2; i < chislo; ++i) {
+            if (chislo % i == 0)
+                chek = false;
+        }
+        if (chek == true)
+            return chislo;
+    }
 }
 
-uint64_t sumPrime(uint64_t hbound)
-{
-	uint64_t sum = 0;
-	for (int i = 1; i < hbound; ++i)
-	{
-		if (hbound > nPrime(i))
-			sum += nPrime(i);
-	}
-	return sum;
+uint64_t sumPrime(uint64_t hbound) {
+    uint64_t sum = 0;
+    if (hbound < 3)
+        return 0;
+    for (int i = 2; i < hbound; ++i) {
+        bool chek = true;
+        for (int j = 2; j < i; ++j) {
+            if (i % j == 0)
+                chek = false;
+        }
+        if (chek == true) {
+            sum += i;
+        }
+    }
+    return sum;
 }
