2178a896883b4290416301d6abc8bc0bfcf6e980
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-03-30 20:20:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,9 @@
 #include <cstdint>
 #include "alg.h"
 
+
 bool checkPrime(uint64_t value) {
+// вставьте код функции
     bool chek = true;
     if (value == 2)
         return chek;
@@ -14,6 +16,7 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
+// вставьте код функции
     uint64_t  chislo = 1;
     int num = 0;
     while (true) {
@@ -33,6 +36,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
+// вставьте код функции
     uint64_t chislo = value;
     if (value < 2)
         return 2;
@@ -49,6 +53,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
+// вставьте код функции
     uint64_t sum = 0;
     if (hbound < 3)
         return 0;
